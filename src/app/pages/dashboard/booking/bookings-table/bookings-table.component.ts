83345af880002f6ece0d4e1b3989973df55ae2c5
Victor Durand <vic20016@gmail.com>
2020-05-14 11:13:39 +0200

    fix: error in service

--- src/app/pages/dashboard/booking/bookings-table/bookings-table.component.ts
+++ src/app/pages/dashboard/booking/bookings-table/bookings-table.component.ts
@@ -22,20 +22,7 @@ export class BookingsTableComponent implements OnInit {
     this.getMyBooking();
   }
 
-  async getAllBookings() {
-    this.bookings = await this.bookingService
-      .getAllBooking(this.per_page, this.page)
-      .toPromise();
-  }
-  async getBookingById(id) {
-    const booking: any = await this.bookingService.getById(id);
-    this.modalService.create({
-      nzComponentParams: {
-        booking: booking,
-      },
-      nzContent: BookingModalComponent,
-    });
-  }
+
   async getMyBooking() {
     this.bookings = await this.bookingService.getMyBookings().toPromise();
   }
